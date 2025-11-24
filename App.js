9621727cce9392161bb51f97048539bffafc3561
MauroS-Lima <mauro.sergio.g.lima@gmail.com>
2025-11-24 02:17:42 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Preload from './src/screens/Preload';
+import SignIn from './src/screens/SignIn';
+import Home from './src/screens/Home';
+import UserContextProvider from './src/contexts/UserContext';
+
+const Stack = createStackNavigator()
+
+function App() {
+  return (
+    <UserContextProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Preload"
+          screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Preload" component={Preload}/>
+        <Stack.Screen name="SignIn" component={SignIn}/>
+        <Stack.Screen name="Home" component={Home}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </UserContextProvider>
+  );
+}
+
+export default App;
