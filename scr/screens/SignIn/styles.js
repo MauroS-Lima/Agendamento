908eee4620899322d9e247bbc3ce3e46fbe71a20
MauroS-Lima <mauro.sergio.g.lima@gmail.com>
2025-11-24 02:37:28 -0300

    Create styles.js

---
+++ scr/screens/SignIn/styles.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#63c2d1",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+  borderWidth: 1,
+  borderColor: "#999",
+  borderRadius: 10,
+  padding: 10,
+  marginBottom: 10,
+  width: "80%",
+  backgroundColor: "#fff",
+},
+  logo: {
+    width: 100,
+    height: 90,
+    marginBottom: 20,
+  },
+});
