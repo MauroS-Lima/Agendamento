18441970b7c01ec6456956c75f9e20e94d08a43a
MauroS-Lima <mauro.sergio.g.lima@gmail.com>
2025-11-30 04:00:37 -0300

    Update Selector.js

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -13,11 +13,13 @@ const Selector = ({day, hour, active, onToggle}) => {
         marginVertical: 2,
         backgroundColor: color,
         padding: 3,
-        borderRadius: 4,
+        borderRadius: 10,
+        height:60,
       }}
     >
     { hour < 6 ? <Text style={styles.time}>{hour+7}:30</Text>:<Text style={styles.time}>{hour+7}:00</Text>}
-    {(active != "Disponível" && active != "Indisponível") && active}
+    {active==='Disponível'||active==='Indisponível' ? <Text style={styles.time}>Status:</Text> : <Text style={styles.time}>Paciente:</Text>}
+    <Text style={styles.time}>{active}</Text>
     </TouchableOpacity>
   )
 }
