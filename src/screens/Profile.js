0aa7e2340b8aec68105d7fd8b2a6be8f038e5629
MauroS-Lima <mauro.sergio.g.lima@gmail.com>
2025-12-01 02:16:57 -0300

    Update Profile.js

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import {Text,View} from 'react-native'
+import {Text,View,Button} from 'react-native'
 import styles from '../styles';
+import AsyncStorage from "@react-native-community/async-storage"
 
 function Profile({navigation}) {
+  const logOff = async()=>{
+    navigation.reset({routes:[{name:'SignIn'}]})
+    await AsyncStorage.clear()
+  }
+  const dados = async ()=>{
+    await AsyncStorage.multiGet(['name', 'docName'])
+  }
   return(
      <View style = {styles.container}>
-      <Text>Profile</Text>
+     <View style = {styles.profile}>
+     <Text>Nome: </Text>
+     
+      <Button title='Logoff' color='red' onPress={() => logOff()} />
+      </View>
      </View>
   );
 }
 
 export default Profile;
+//{dados[0][1]===dados[1][1]?<Text>Psicologo(a)</Text>:<Text>Psicologo(a): {dados[1][1]}</Text>}
