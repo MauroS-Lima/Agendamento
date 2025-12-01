3e4b9650ed4e2c6f2b8cf958b12c7a340dc4c059
MauroS-Lima <mauro.sergio.g.lima@gmail.com>
2025-12-01 02:15:23 -0300

    Update Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,25 +1,30 @@
 import React, {useState, useEffect} from 'react';
 import {Text, View, ScrollView, TouchableOpacity} from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage'
 
 import styles from '../styles';
 import Api from  '../Api';
 import Card from '../components/Card'
+import WeekView from '../components/WeekView'
 
-const test = {data: '28 Dez', hora: '12:00', subject: 'disponivel'}
+import Schedule from '../MockData/Schedule'
+
+const test = {data: '28 Dez', hora: '12:00', subject: 'Disponível'}
 
 const getSchedule = async() => {
   setLoading(true);
-  setList([]);
-
-  let res = await Api.getSchedule();
-  if(res.error == '') {
-    setList(res.data);
-  } else{ alert('Error: ' +res.error)}
+  const dados = async ()=>{
+    await AsyncStorage.multiGet(['name', 'docName'])
+  }
+  //let res = await Api.getSchedule();
+  //if(res.error == '') {
+  //  setList(res.data);
+  //} else{ alert('Error: ' +res.error)}
 
   setLoading(false);
 }
 
-const Section = ({subject, status, time }) =>{
+const Section = () =>{
   return(
     <View style={styles.scrollerTab}>
       <Text>{test.data}</Text>
@@ -29,6 +34,8 @@ const Section = ({subject, status, time }) =>{
   )
 }
 
+//contentContainerStyle = {styles.scrollerCont}>
+
 function Home({navigation}) {
 
   useEffect(()=>{
@@ -36,13 +43,11 @@ function Home({navigation}) {
 }, []);
 
   return(
-     <View contentContainerStyle = {styles.container}>
-      <Text>Home</Text>
-      <ScrollView style = {styles.scroller}>
-      <Section time='time' subject ='subject' status = 'status'/>
-      <Card props= {test}/>
+      <ScrollView style = {styles.scroller} contentContainerStyle = {styles.scrollerCont}>
+      <Section />
+      <ScrollView horizontal><Card props= {test}/></ScrollView>
+      
       </ScrollView>
-     </View>
   );
 }
 
