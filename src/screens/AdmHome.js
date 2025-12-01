1d8658f6ea205595aaea9c9aa684f3677e176ab7
MauroS-Lima <mauro.sergio.g.lima@gmail.com>
2025-12-01 02:14:48 -0300

    Update AdmHome.js

--- src/screens/AdmHome.js
+++ src/screens/AdmHome.js
@@ -3,14 +3,14 @@ import {Text,View,Button} from 'react-native'
 
 import styles from '../styles';
 import WeekView from '../components/WeekView'
-//import Schedule from '../MockData/Schedule'
+import Schedule from '../MockData/Schedule'
 
 function AdmHome({navigation}) {
-  //const [Storage, setStorage] = useState(Schedule)
+  const [Storage, setStorage] = useState(Schedule)
   return(
      <View style = {{flex:1}}>
       <Text>Admin</Text>
-      <WeekView />  //Storage?
+      <WeekView />
       <Button title='Logoff' onPress={() => navigation.navigate('Preload')} />
      </View>
   );
