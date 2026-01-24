e1fdf2b7220006313a044d804f098e9f303870f9
MauroS-Lima <mauro.sergio.g.lima@gmail.com>
2026-01-24 16:12:17 -0300

    Update MainTab.js

--- src/MainTab.js
+++ src/MainTab.js
@@ -8,32 +8,37 @@ import {UserContext} from './contexts/UserContext'
 import Vagas from './screens/Vagas';
 import Agenda from './screens/Agenda';
 import Profile from './screens/Profile';
+import PatientManager from './screens/PatientManager'
 
 const Tab = createBottomTabNavigator();
 
 
 
 function MainTab({navigation}) {
-  //const{state=user} = useContext(UserContext);
+  const { data:user } = useContext(UserContext);
   return(
     <Tab.Navigator screenOptions={{
       tabBarStyle: { backgroundColor: "#fff", height:60 },
       tabBarInactiveTintColor:'#888',
       tabBarActiveTintColor:"#e77",
-       }} // debug ->   initialRouteName="Profile" 
+       }} //initialRouteName="Perfil" 
        >
 
-       <Tab.Screen name='Agenda' component={Agenda} 
-      options={{tabBarIcon:({color})=>(<MaterialCommunityIcons name="calendar-account" color={color} size={26} />)}} />
-
-      <Tab.Screen name='Profile' component={Profile} 
+      <Tab.Screen name='Perfil' component={Profile} 
       options={{tabBarIcon:({color})=>(<MaterialCommunityIcons name="account" color={color} size={26} />)}} />
+
+      <Tab.Screen name='Agenda' component={Agenda} 
+      options={{tabBarIcon:({color})=>(<MaterialCommunityIcons name="calendar-account" color={color} size={26} />)}} />
 
       <Tab.Screen name='Vagas' component={Vagas} 
       options={{tabBarIcon:({color})=>(<MaterialCommunityIcons name="calendar-edit" color={color} size={26} />)}} />
 
+      <Tab.Screen name='Usuarios' component={PatientManager} 
+      options={{tabBarIcon:({color})=>(<MaterialCommunityIcons name="account-edit" color={color} size={26} />)}} />
+
     </Tab.Navigator>
   );
 }
-
+//{ user.doc === user.name ? }
+// account-edit
 export default MainTab;
