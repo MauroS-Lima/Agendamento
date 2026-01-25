d9294599b157611cfde6d787e24556e2b5093616
MauroS-Lima <mauro.sergio.g.lima@gmail.com>
2026-01-25 19:27:37 -0300

    Create RSelector.js

---
+++ src/components/RSelector.js
@@ -0,0 +1,33 @@
+import { Text, TouchableOpacity, View, Modal } from 'react-native';
+import React, { useState, useContext } from 'react';
+
+import styles from '../styles';
+import Card from './Card'
+
+const PSelector = ({prop}) => {
+  //console.log(prop)    paciente 
+  const [modal, setModal] = useState(false);
+  const color =(prop.active==='Disponível' ? 'green' : prop.active==='Indisponível' ? '#eee' : 'red')
+  return (
+    <View><TouchableOpacity
+      onPress={() => { if( prop.active !== 'Indisponível' ){ setModal(true) } } }
+      style={{
+        marginVertical: 1,
+        backgroundColor: color,
+        paddingVertical: 7,
+        paddingHorizontal: 12,
+        borderRadius: 10,
+        marginHorizontal: 4, 
+        justifyContent: "center",
+      }}
+    > 
+    {!(prop.active==='Disponível'||prop.active==='Indisponível') ? <Text style={styles.time}>Ocupado</Text> : prop.hour < 6 ? <Text style={styles.time}>{prop.hour+7}:30</Text>:<Text style={styles.time}>{prop.hour+7}:00</Text>}
+    
+    </TouchableOpacity><Modal transparent={true} visible={modal} onRequestClose={()=>{setModal(false)}}><Card props={prop} power={setModal} /></Modal></View>
+  )
+}
+
+export default PSelector;
+//<Text style={styles.time}>{active}</Text>
+//prop.onToggle(prop)
+
