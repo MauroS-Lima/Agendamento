dce2a92d4025c5c21005d2ae6793605c9b15143c
MauroS-Lima <mauro.sergio.g.lima@gmail.com>
2026-01-25 19:26:29 -0300

    Update SchedView.js

--- src/components/SchedView.js
+++ src/components/SchedView.js
@@ -8,7 +8,7 @@ import { UserContext } from '../contexts/UserContext'
 import Butao from './Butao'
 import Schedule from '../MockData/Schedule'
 
-const dayStr = ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom'];
+const dayStr = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'];
 const textStr = ['Cronograma da semana', 'Selecione uma data disponivel para reagendar'];
 
 
@@ -17,11 +17,15 @@ function SchedView({mode=0}) {
   const { data:user} = useContext(UserContext);
   const { dispatch: userDispatch } = useContext(UserContext);
 
-  
   const [Changes, setChanges] = useState(user.alterations);
   const [Lista, setLista] = useState(user.weekly);
 
-  //const pacientesLivres = () => {setPacientes(user.pacientes.filter((u) => Lista.some((x) => x.some((y) => y===u))))}
+  const now = new Date(2026,0,27)
+
+  const data = (dia) => {
+    num = new Date(new Date(now.valueOf()+((dia-now.getDay())*86400000)))
+    return(num)    
+  }
 
   const saveDisp = async() => {
     userDispatch({type:'reschedule', payload: {alterations: Changes} });
@@ -40,7 +44,7 @@ function SchedView({mode=0}) {
 
     sending=JSON.stringify(Lista)
 
-    AsyncStorage.removeItem(user.name) 
+    AsyncStorage.removeItem(user.name)
     };
 
   const pickSlot = (props) => {
@@ -51,7 +55,6 @@ function SchedView({mode=0}) {
       );
     }
   };
-
   const clearSlot = (props) => {
     setLista((s) =>
       s.map((dia) => dia.map(
@@ -64,42 +67,35 @@ function SchedView({mode=0}) {
   return (
     <View style={styles.weekly} >
 
-      <Text style={{ fontWeight: 'bold', fontSize: 18, marginBottom: 14, textAlign: 'center' }}>
+      <Text style={styles.header}>
         {textStr[mode]}:
       </Text>
 
-      <ScrollView horizontal style={{alignItems: "left"}} >
-        <View style={{ flexDirection: 'row', alignItems: "left", padding: 5, marginBottom: 10, backgroundColor: "#0EADBE",}}>
+      <ScrollView horizontal style={{ marginTop: 14, padding: 5, backgroundColor: "#0EADBE",}} >        
           {[...Array(7)].map((_, dia) => (
-            <View
-              style={styles.day}
-            >
-              <Text style={{ fontWeight: 'bold', textAlign: 'center', marginBottom: 5}}>
-                {dayStr[dia]}
+
+            <View style={dia === now.getDay() ? {...styles.day, borderWidth: 3, } : 
+            mode === 1 && dia<now.getDay() ? {...styles.day, backgroundColor: '#ccc'} : styles.day} >
+
+              <Text style={styles.title}>
+                {dayStr[dia]}{'\n'}{JSON.stringify(data(dia).getDate()) + '/' + JSON.stringify(data(dia).getMonth() + 1)}
               </Text>
+
               {[...Array(14)].map((_, i) => {
                 const hora = 0 + i;
             
                 const ativo = Lista[dia][hora]
                 
-                const x = {day:dia, hour: hora, active:ativo, paciente: user.pacientes[0], diaSem: dayStr, type: mode,
-                clear: clearSlot, onToggle: pickSlot
-                }
+                const x = {day:dia, hour: hora, active: ativo, doc: user.name === user.doc, diaSem: dayStr, type: mode,
+                clear: clearSlot, onToggle: pickSlot }
 
-                if(mode===0){
-                  return( <Display prop={x}/> )
-                } else( <Display prop={x}/> )
+                if( mode === 1 && dia>now.getDay() ) { return( <Display prop={x}/> )
+                } else return(<Display prop={x}/>) 
                 
               })}
             </View>
-          ))}
-        </View>
+          ))}        
       </ScrollView> 
-
-      { mode !== 0 ? <View style={{flexDirection: 'row'}}><Butao text={'Salvar'} onClick={saveDisp}/> 
-      <Butao text={'Limpar'} color = {'red'} onClick={() => {if(mode===1){
-        user.pacientes.map((a) => {clearSlot({ paciente: a })})
-        }else{clearDisp()}}}/></View> : <Text></Text>}
 
     </View>
   );
