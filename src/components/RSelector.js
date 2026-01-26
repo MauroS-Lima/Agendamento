6bc6f89ff324dd5d277559242ef2f3b648031378
MauroS-Lima <mauro.sergio.g.lima@gmail.com>
2026-01-26 06:37:31 -0300

    Update RSelector.js

--- src/components/RSelector.js
+++ src/components/RSelector.js
@@ -8,22 +8,24 @@ const PSelector = ({prop}) => {
   //console.log(prop)    paciente 
   const [modal, setModal] = useState(false);
   const color =(prop.active==='Disponível' ? 'green' : prop.active==='Indisponível' ? '#eee' : 'red')
+  const padd = (!(prop.active==='Disponível'||prop.active==='Indisponível') ? 2 : 13)
+  const show = ( prop.doc ? prop.active : 'Ocupado')
   return (
-    <View><TouchableOpacity
+    <View ><TouchableOpacity
       onPress={() => { if( prop.active !== 'Indisponível' ){ setModal(true) } } }
       style={{
         marginVertical: 1,
         backgroundColor: color,
         paddingVertical: 7,
-        paddingHorizontal: 12,
-        borderRadius: 10,
-        marginHorizontal: 4, 
+        paddingHorizontal: padd,
         justifyContent: "center",
+
+
       }}
     > 
-    {!(prop.active==='Disponível'||prop.active==='Indisponível') ? <Text style={styles.time}>Ocupado</Text> : prop.hour < 6 ? <Text style={styles.time}>{prop.hour+7}:30</Text>:<Text style={styles.time}>{prop.hour+7}:00</Text>}
+    {!(prop.active==='Disponível'||prop.active==='Indisponível') ? <Text style={styles.time}>{show}</Text> : prop.hour < 6 ? <Text style={styles.time}>{prop.hour+7}:30</Text>:<Text style={styles.time}>{prop.hour+7}:00</Text>}
     
-    </TouchableOpacity><Modal transparent={true} visible={modal} onRequestClose={()=>{setModal(false)}}><Card props={prop} power={setModal} /></Modal></View>
+    </TouchableOpacity><Modal transparent={true} visible={modal} onRequestClose={()=>{setModal(false)}}><Card props={{...prop, power: setModal}} /></Modal></View>
   )
 }
 
