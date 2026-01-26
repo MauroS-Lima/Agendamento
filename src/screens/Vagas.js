df20ac9cf8fdb3a0a6279a8baefa00b0af62b03b
MauroS-Lima <mauro.sergio.g.lima@gmail.com>
2026-01-25 23:23:32 -0300

    Update Vagas.js

--- src/screens/Vagas.js
+++ src/screens/Vagas.js
@@ -1,10 +1,9 @@
 import React, {useState, useEffect, useContext} from 'react';
-import {Text, View, ScrollView, TouchableOpacity} from 'react-native'
+import { ScrollView } from 'react-native'
 import AsyncStorage from '@react-native-community/async-storage'
 
 import styles from '../styles';
 import Api from  '../Api';
-import Card from '../components/Card'
 import { UserContext } from '../contexts/UserContext'
 import WeekView from '../components/WeekView'
 
@@ -20,7 +19,7 @@ function Vagas({navigation}) {
 
   return(
       <ScrollView style = {styles.scroller} contentContainerStyle = {styles.scrollerCont}>
-      <WeekView mode={2}/>
+      <WeekView mode={0}/>
       </ScrollView>
   );
 }
