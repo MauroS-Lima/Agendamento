005ab9f92ae94e46ac60edd36778f4c65a5655c1
MauroS-Lima <mauro.sergio.g.lima@gmail.com>
2026-01-25 23:39:00 -0300

    Update PatientManager.js

--- src/screens/PatientManager.js
+++ src/screens/PatientManager.js
@@ -12,15 +12,14 @@ function Manager({navigation}) {
   const {dispatch: userDispatch}=useContext(UserContext)
   const { data: user } = useContext(UserContext);
 
-  const d = user.users.filter((u) => u.name === u.docName )
-  const docs = d.map((x) => x.name)
+  //const d = user.users.filter((u) => u.name === u.docName )
+  //const docs = d.map((x) => x.name)
 
   const p = user.users.filter((u) => u.name !== u.docName )
   const pacs = p.map((x) => x.name)
 
   const [ nome, setNome ] = useState('');
   const [ senha, setSenha ] = useState('');
-  const [ psy, setPsy ] = useState(docs[0]);
   const [ usuario, setUsuario ] = useState(pacs[0]);
   const [ modal, setModal ] = useState(false);
 
@@ -28,10 +27,10 @@ function Manager({navigation}) {
 
     //let json = await Api.logInCreate(usuario,senha,psy);
 
-    if ( nome != '' && senha != '' && psy != ''){
+    if ( nome != '' && senha != '' ){
       
       if( ! user.users.some((u) => u.name===nome ) ){ 
-        Users = [...user.users, { name: nome, password: senha, docName: psy, token: 'valido' }]        
+        Users = [...user.users, { name: nome, password: senha, docName: user.name, token: 'valido' }]        
         userDispatch({
           type:'addUser',
           payload: Users
@@ -59,10 +58,8 @@ const loginDelete = async() => {
     <View style = {{flex: 1, backgroundColor: "#63c2d1", justifyContent: "center"}}>
 
       <View style = {{ alignItems: "center", justifyContent: "center", marginTop: 20 }}>
-        <Text style={styles.header}>Adicionar paciente:</Text>      
-        <ScrollView horizontal style={{ flexDirection: 'row', padding :5, }}> <Text style={styles.text}>Psicólogo(a): </Text> 
-        {docs.map((a) => ( <Butao text={a} onClick={() => setPsy(a)} color={ a===psy ? 'blue' : '#888' } size={6} /> ))} 
-        </ScrollView>
+        <Text style={{...styles.header, marginBottom: 15}}>Adicionar paciente:</Text>      
+
         <TextInput 
           style={styles.input}
          placeholder='nome'
@@ -82,9 +79,9 @@ const loginDelete = async() => {
       </View>
 
     <View style = {{ alignItems: "center", justifyContent: "center", marginTop: 20 }}>
-        <Text style={styles.header}>Remover paciente:</Text>
+        <Text style={{...styles.header, marginBottom: 5}}>Remover paciente:</Text>
         <ScrollView horizontal style={{ flexDirection: 'row', padding :5, marginBottom: 10 }}>
-        {pacs.map((a) => ( <Butao text={a} onClick={() => setUsuario(a)} color={ a === usuario ? 'black' : 'grey' } size={6} /> ))} 
+        {user.pacientes.map((a) => ( <Butao text={a} onClick={() => setUsuario(a)} color={ a === usuario ? 'black' : 'grey' } size={6} /> ))} 
         </ScrollView> <Butao text={'Excluir'} color='red' onClick={() => loginDelete() }/>
       </View>
 
